32f9af6aa5d5f96fbab3c429b9aad4ccd5778267
Li-aung 'Lewis' Yip <liaung@gmail.com>
2015-07-05 18:04:16 +0800

    WIP...

--- outlook_dump.py
+++ outlook_dump.py
@@ -13,22 +13,9 @@ import os
 import logging
 import time
 
-
-def set_up_logging ():
-    # set up logging
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
-    # log all messages to file
-    l1 = logging.FileHandler('log-%s.txt' % time.strftime("%Y-%m-%dT%H%M%S"))
-    l1.setLevel(logging.DEBUG)
-    # log warnings and errors to console
-    l2 = logging.StreamHandler()
-    l2.setLevel(logging.DEBUG)
-    fm = logging.Formatter('%(levelname)s: %(message)s')
-    for handler in [l1,l2]:
-        handler.setFormatter(fm)
-        root_logger.addHandler(handler)
-    logging.info("Starting up....")
+# ---------------------------------------------------------------------------- #
+# Function definitions
+# ---------------------------------------------------------------------------- #
 
 def get_subfolders( folder, f_list = list() ):
     # Return a flat list of Outlook `Folder` objects,
@@ -72,7 +59,7 @@ def user_select_filesystem_dir():
     return dir
 
 def get_local_timezone():
-    TZ = "Australia/Perth" #TODO: Make this configurable
+    TZ = "Australia/Perth" #TODO: this function is obsolete
     return pytz.timezone(TZ)
     logging.info("Using timezone %s" % TZ)
 
@@ -85,7 +72,7 @@ def make_directories(directory_path):
         assert os.path.isdir (directory_path)
         logging.info("Created folder %s." % directory_path)
 
-def get_mailitem_utc_time_string ( mailitem, local_timezone ):
+def get_mailitem_utc_time ( mailitem, local_timezone ):
     # mailitem is an Outlook.MailItem.
     # Times are returned as `pywintypes.Time` objects.
     # See http://timgolden.me.uk/python/win32_how_do_i/use-a-pytime-value.html.
@@ -99,68 +86,237 @@ def get_mailitem_utc_time_string ( mailitem, local_timezone ):
         logging.warning("MailItem didn't have a ReceivedTime. Using CreationTime instead. (Mail item: %s, CreationTime: %s)" % (subject, timestamp))
     tz_aware_time = datetime.datetime.fromtimestamp(timestamp,local_timezone)
     utc_time = tz_aware_time.astimezone(pytz.utc)
+    return utc_time
+
+def get_mailitem_utc_time_string ( utc_time ):
     # Like ISO format, but without the :'s (not OK for filenames) and
     # without the time-zone ID at the end.
-    utc_time_string = utc_time.__format__("%Y-%m-%dT%H%M%SZ")
-    return utc_time_string
+    return utc_time.__format__("%Y-%m-%dT%H%M%SZ")
 
+def ol_category_exists ( ol_namespace, category_name ):
+    return not (ol_namespace.Categories[category_name] is None)
 
-set_up_logging()
-logging.info("Opening Outlook Application...")
-ol_application = win32com.client.Dispatch("Outlook.Application")
-ol_namespace = ol_application.GetNamespace("MAPI") # Equivalent to ol_application.Session
+def create_ol_category ( ol_namespace, category_name ):
+    # Adds a new category. The category colour is the next un-used colour.
+    # The category hot-key is not assigned.
+    if not ol_category_exists( ol_namespace, category_name ):
+        ol_namespace.Categories.Add (category_name)
 
-selected_ol_folder = user_select_outlook_folder(ol_namespace)
-ol_folder_list = get_subfolders(selected_ol_folder)
-save_to_folder = user_select_filesystem_dir()
-local_timezone = get_local_timezone()
-num_processed = 0
-num_saved = 0
+def email_in_ol_category (mailitem, category_name):
+    return (category_name in mailitem.Categories.split(", "))
 
-for folder in ol_folder_list:
-    ol_folder_path_str = folder.FolderPath # Example: \\Outlook Data File\J9 Administrivia\Expenses
-    logging.info("Entering folder %s" % ol_folder_path_str)
-    ol_folder_path_parts = ol_folder_path_str.split("\\")[1:] # [1:] to skip empty part due to "\\" at start
-    ol_folder_path = os.sep.join( [slugify(p) for p in ol_folder_path_parts] ) # clean naughty characters
+def set_ol_category (mailitem, category_name):
+    if True: #not email_in_ol_category(mailitem, category_name):
+        mailitem.Categories = ", ".join((mailitem.Categories, category_name))
+        mailitem.Save()
 
-    dest_folder_path = os.path.join (save_to_folder, ol_folder_path)
-    make_directories(dest_folder_path)
+def unset_ol_category (mailitem, category_name):
+    cats = str(mailitem.Categories).split(", ")
+    mailitem.Categories = ", ".join( (c for c in cats if c != category_name) )
 
-    for mi in folder.Items: #mi = MailItem
-        if num_processed % 100 == 0:
-            logging.info("... %i emails processed ..." % num_processed)
-        num_processed += 1
+# ---------------------------------------------------------------------------- #
+# User settings load/save
+# ---------------------------------------------------------------------------- #
 
-        # "IPM.Note" is a normal email message.
-        # "IPM.Note.EnterpriseVault.Shortcut" is an email message archived by Enterprise Vault.
-        # other types include:
-        # IPM.Appointment, IPM.Contact, IPM.Schedule.Meeting.Resp.Pos, and so on.
-        if not mi.MessageClass.startswith("IPM.Note"):
-            logging.debug("Ignoring item `%s` in folder `%s` of type `%s`" %
-                          (mi.Subject, ol_folder_path_str, mi.MessageClass))
-            continue
+SETTINGS_FILE_NAME = "easygui_settings.dat"
+class Settings(easygui.EgStore):
+    def __init__(self, filename):
+        logging.info("Initialising settings...")
+        self.filename = filename
+        self.timezone_name = ""
+        self.debug_mode = ""
+        self.days_old = -1
+        self.dest_dir = ""
 
-        raw_subject = mi.Subject
-        subject = slugify(raw_subject[:100])
-        if raw_subject != subject:
-            logging.debug("Raw name `%s` was cleaned to `%s`" %
-                          (raw_subject, subject))
+        if os.path.isfile(self.filename):
+            logging.info("Config file %s found. Attempting to load..."
+                         % SETTINGS_FILE_NAME)
+            self.restore()
+        else:
+            logging.info("No config file %s found. Asking user for settings..."
+                         % SETTINGS_FILE_NAME)
+            self.enter_settings()
 
-        utc_time_string = get_mailitem_utc_time_string (mi, local_timezone)
+        while True:
+            logging.info("Validating settings...")
+            if self.settings_valid() and self.user_confirm_settings():
+                break
+            logging.info("Settings invalid. Re-asking user for settings...")
+            self.enter_settings()
 
-        file_name = utc_time_string + ' ' + subject + ".MSG"
-        file_path = os.path.join ( dest_folder_path , file_name )
+        logging.info("Storing settings to file %s..." % SETTINGS_FILE_NAME)
+        self.store()
 
-        logging.debug("Trying to save %s" % file_path)
+    def settings_valid(self):
         try:
-            mi.SaveAs ( file_path, 9 ) # Magic number 9 = olUnicodeMsg.
-            num_saved += 1
+            assert type (self.timezone_name) is str
+            assert self.timezone_name != ""
+            assert self.debug_mode in ("Normal mode", "Debug mode")
+            assert type (self.days_old) is int
+            assert self.days_old >= 0
+            assert self.dest_dir != ""
+            assert os.path.isdir(self.dest_dir)
+            return True
+        except AttributeError, AssertionError:
+            logging.exception("Invalid setting.")
+            return False
+
+    def enter_settings(self):
+        s1 = "What is your local time zone? (i.e. Australia/Perth.) Used to determine the UTC/GMT time for file naming purposes."
+        self.timezone_name = easygui.choicebox(s1,
+                                               "Choose local time zone",
+                                               pytz.common_timezones)
+
+        s2 = "Enable debug mode? (Displays very detailed program execution information on screen.)"
+        self.debug_mode = easygui.buttonbox(s2,
+                                            "Debug mode?",
+                                            ("Normal mode","Debug mode"))
+
+        s3 = "Save emails older than X days: (default 60 days; 0 for all emails regardless of age.)"
+        self.days_old = easygui.integerbox(s3,"Email age?",default=60,
+                                           lowerbound=0, upperbound=99999999)
+
+        self.dest_dir = easygui.diropenbox("Pick a folder to save the email .msg files to.",
+                                           "Choose backup destination",
+                                           "%HOMEPATH%")
+
+    def user_confirm_settings(self):
+        s1 = """Your settings are:
+Time zone: `%s`
+Debug mode: `%s`
+Save emails older than `%i` days
+Destination directory: `%s`
+
+Use these settings, or select new settings?
+"""
+        s1 = s1 % ( self.timezone_name,
+                    self.debug_mode,
+                    self.days_old,
+                    self.dest_dir)
+        logging.info(s1)
+        d = easygui.buttonbox(s1,"Use these settings?",("Use these settings","New settings"))
+        logging.info("User input: %s" % d)
+        return (d == "Use these settings")
+
+
+
+
+
+# ---------------------------------------------------------------------------- #
+# Main script
+# ---------------------------------------------------------------------------- #
+
+#TODO: Set up GUI configuration interface.
+#TODO: Pywin32 packaging.
+
+# set up logging
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+# log all messages to file. 10 second delay to avoid nasty disk seeking.
+l1 = logging.FileHandler('log-%s-detailed.txt' % time.strftime("%Y-%m-%dT%H%M%S"), encoding="utf-8", delay=10)
+l1.setLevel(logging.DEBUG)
+# log only info and above to a second file.
+l2 = logging.FileHandler('log-%s-summary.txt' % time.strftime("%Y-%m-%dT%H%M%S"), encoding="utf-8", delay=10)
+l2.setLevel(logging.INFO)
+# log to console - if in debug mode, log everything. Otherwise, only INFO and above.
+l3 = logging.StreamHandler()
+l3.setLevel(logging.INFO) # TODO: Switch between INFO and DEBUG
+fm = logging.Formatter('%(levelname)s: %(message)s')
+for handler in [l1,l2,l3]:
+    handler.setFormatter(fm)
+    root_logger.addHandler(handler)
+logging.info("Starting up....")
+
+
+try:
+    settings = Settings(SETTINGS_FILE_NAME)
+
+    logging.info("Opening Outlook Application...")
+    ol_application = win32com.client.Dispatch("Outlook.Application")
+    ol_namespace = ol_application.GetNamespace("MAPI") # Equivalent to ol_application.Session
+
+    selected_ol_folder = user_select_outlook_folder(ol_namespace)
+    save_to_folder = settings.dest_dir # TODO: Remove this tautology
+
+    ol_folder_list = get_subfolders(selected_ol_folder)
+
+    local_timezone = get_local_timezone()
+    now = datetime.datetime.now(pytz.utc)
+
+    CATEGORY_NAME = "Saved as MSG"
+    create_ol_category(ol_namespace,CATEGORY_NAME)
+
+    num_already_archived = 0
+    num_processed = 0
+    num_saved = 0
+
+    for folder in ol_folder_list:
+        ol_folder_path_str = folder.FolderPath # Example: \\Outlook Data File\J9 Administrivia\Expenses
+        logging.info("Entering folder %s" % ol_folder_path_str)
+        ol_folder_path_parts = ol_folder_path_str.split("\\")[1:] # [1:] to skip empty part due to "\\" at start
+        ol_folder_path = os.sep.join( [slugify(p) for p in ol_folder_path_parts] ) # clean naughty characters
+
+        dest_folder_path = os.path.join (save_to_folder, ol_folder_path)
+        make_directories(dest_folder_path)
+
+        for mi in folder.Items: #mi = MailItem
+            num_processed += 1
+            raw_subject = mi.Subject
+            # "IPM.Note" is a normal email message.
+            # "IPM.Note.EnterpriseVault.Shortcut" is an email message archived by Enterprise Vault.
+            # other types include:
+            # IPM.Appointment, IPM.Contact, IPM.Schedule.Meeting.Resp.Pos, and so on.
+            if not mi.MessageClass.startswith("IPM.Note"):
+                logging.debug("Ignoring item `%s` in folder `%s` of type `%s`" %
+                              (raw_subject, ol_folder_path_str, mi.MessageClass))
+                continue
+
+            if email_in_ol_category(mi, CATEGORY_NAME):
+                logging.debug("Item `%s` already archived (`%s`). Skipping." %
+                              (raw_subject, CATEGORY_NAME))
+                num_already_archived += 1
+                continue
+
+            utc_time = get_mailitem_utc_time(mi, local_timezone)
+            delta = now - utc_time
+            if delta.days < 60: #TODO: Fix hardcoding
+                logging.debug("Item `%s` of date `%s` is less than %i days old. Skipping." %
+                              (raw_subject, utc_time, 60)) # TODO: Fix hardcoding
+                continue
+            else:
+                logging.debug("Item `%s` of date `%s` is %i days old, archiving..." %
+                              (raw_subject, utc_time, delta.days)) # TODO: Fix hardcoding
+
+            utc_time_string = get_mailitem_utc_time_string (utc_time)
+
+            subject = slugify(raw_subject[:100])
+            if raw_subject != subject:
+                logging.debug("Raw name `%s` was cleaned to `%s`" %
+                              (raw_subject, subject))
+
+            file_name = utc_time_string + ' ' + subject + ".MSG"
+            file_path = os.path.join ( dest_folder_path , file_name )
+
+            logging.debug("Trying to save %s" % file_path)
+            try:
+                mi.SaveAs ( file_path, 9 ) # Magic number 9 = olUnicodeMsg.
+                # set_ol_category(mi, CATEGORY_NAME)
+                num_saved += 1
+            except pywintypes.com_error:
+                logging.exception("Failure in MailItem.SaveAs().")
+                logging.error("Details... MessageClass: `%s`, FolderPath: `%s`, Subject `%s`, ReceivedTime `%s`, CreationTime `%s`" %
+                              (mi.MessageClass, ol_folder_path_str, raw_subject, mi.ReceivedTime, mi.CreationTime))
+
             if num_saved % 50 == 0:
                 logging.info("... %i emails saved to .msg ..." % num_saved)
-        except pywintypes.com_error:
-            logging.exception("Failure in MailItem.SaveAs().")
-            logging.error("Details... MessageClass: `%s`, FolderPath: `%s`, Subject `%s`, ReceivedTime `%s`, CreationTime `%s`" %
-                          (mi.MessageClass, ol_folder_path_str, raw_subject, mi.ReceivedTime, mi.CreationTime))
 
+            if num_processed % 100 == 0:
+                logging.info("... %i emails processed ..." % num_processed)
 
-logging.info("DONE. Processed: %i, Saved: %i" % (num_processed, num_saved))
+    logging.info("DONE. Processed: %i, Saved: %i, Already archived (skipped): %i" %
+                 (num_processed, num_saved, num_already_archived))
+
+    raw_input("Press any key to exit...")
+
+except Exception:
+    logging.exception("Fatal error in main program.")
